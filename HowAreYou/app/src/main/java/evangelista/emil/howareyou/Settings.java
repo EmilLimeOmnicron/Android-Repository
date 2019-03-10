621a5ac79c833e608cefa2303c94e301200b4c87
EmilLimeOmnicron <EmilLimeOmnicron@users.noreply.github.com>
2019-03-10 07:36:08 -0700

    Initial commit

---
+++ HowAreYou/app/src/main/java/evangelista/emil/howareyou/Settings.java
@@ -0,0 +1,16 @@
+package evangelista.emil.howareyou;
+
+/**
+ * Created by Emil on 3/10/2019.
+ */
+
+public class Settings {
+
+
+   //set alarm
+
+
+   //set times that alarm wont ring
+
+
+}
---
+++ HowAreYou/app/src/main/java/evangelista/emil/howareyou/Storage.java
@@ -0,0 +1,8 @@
+package evangelista.emil.howareyou;
+
+/**
+ * Created by Emil on 3/10/2019.
+ */
+
+public class Storage {
+}
